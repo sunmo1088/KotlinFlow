a6884a3ae104e483f071f111304de274386ef59f
Sun Mo <sun.mo1088@gmail.com>
2023-08-02 12:55:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kotlinflow/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kotlinflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kotlinflow.ui.theme.KotlinFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinFlowTheme {
+                val viewModel = viewModel<MainViewModel>()
+                val time = viewModel.countDownFlow.collectAsState(initial = 10)
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(
+                        text = time.value.toString(),
+                        fontSize = 30.sp,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
